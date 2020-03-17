d0c7ccd2f7b2e9accdeac7d290492bfe9f350734
jpouyaud <j.pouyaud@f5.com>
2020-03-17 21:19:25 +0100

    Fix an issue when an AWS region =! Paris is choosen documentation updated regarding lab setup time

--- packer/setup-instance.sh
+++ packer/setup-instance.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+sudo wget https://github.com/nginxinc/nginx-asg-sync/releases/download/v0.4-1/nginx-asg-sync-0.4-1.amzn2.x86_64.rpm
+sudo yum install nginx-asg-sync-0.4-1.amzn2.x86_64.rpm -y
+sudo rm nginx-asg-sync-0.4-1.amzn2.x86_64.rpm
+
+sudo nginx -s reload
+
+cat > /tmp/config.yaml <<EOF
 region: eu-west-2
 api_endpoint: http://127.0.0.1:8080/api
 sync_interval_in_seconds: 1
@@ -19,3 +27,7 @@ upstreams:
     max_fails: 1
     fail_timeout: 10s
     slow_start: 0s
+EOF
+
+sudo mv /tmp/config.yaml /etc/nginx/config.yaml
+sudo systemctl enable nginx-asg-sync
